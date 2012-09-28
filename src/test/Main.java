7ab48620791bf67969a62908e89b5f35faae2d21
androidgpr <android.gpr@gmail.com>
2012-09-28 12:50:18 +0200

    Test 3

--- src/test/Main.java
+++ src/test/Main.java
@@ -3,12 +3,8 @@ package test;
 import javax.swing.JOptionPane;
 
 public class Main {
-
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		JOptionPane.showMessageDialog(null,"Äntligen verkar det fungera!!!");
 
